52a9b7c6357e4eaec6487023815fd70df3d625dd
Joe Wu <wubaixing1379@163.com>
2017-03-15 02:57:38 +0800

    finish

--- src/js/root.js
+++ src/js/root.js
@@ -4,6 +4,8 @@ import {Router, Route, hashHistory} from 'react-router';
 import PCIndex from './components/pc_index';
 import PCNewsDetails from './components/pc_news_details';
 import MobileIndex from './components/mobile_index';
+import PCUserCenter from './components/pc_usercenter';
+import MobileUserCenter from './components/mobile_usercenter';
 import MobileNewsDetails from './components/mobile_news_details';
 import MediaQuery from 'react-responsive';
 
@@ -15,12 +17,14 @@ class Root extends Component {
                     <Router history={hashHistory}>
                         <Route path='/' component={PCIndex}/>
                         <Route path='/details/:uniquekey' component={PCNewsDetails}/>
+                        <Route path='/usercenter' component={PCUserCenter}/>
                     </Router>
                 </MediaQuery>
                 <MediaQuery query='(max-device-width: 1224px)'>
                     <Router history={hashHistory}>
                         <Route path='/' component={MobileIndex}/>
                         <Route path='/details/:uniquekey' component={MobileNewsDetails}/>
+                        <Route path='/usercenter' component={MobileUserCenter}/>
                     </Router>
                 </MediaQuery>
             </div>
